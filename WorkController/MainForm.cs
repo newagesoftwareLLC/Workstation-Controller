774c5d5899b7966a45c65fe301864cc487841a7e
Jacob Fliss <newgmobile@gmail.com>
2021-06-30 11:31:04 -0400

    minor name adjustments

--- WorkController/MainForm.cs
+++ WorkController/MainForm.cs
@@ -20,9 +20,9 @@ using Titanium.Web.Proxy;
 
 namespace workController
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
